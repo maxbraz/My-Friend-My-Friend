6502f926792f3715dcaaa2a29b22c80c06f02f86
Max Braz <braz.max@gmail.com>
2017-04-30 14:03:28 -0700

    update db config

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
-// 'mysql://bc321ec3408dd9:a8a236a1@us-cdbr-iron-east-03.cleardb.net/heroku_7617e6dc46c4343?reconnect=true'
+'mysql://bc321ec3408dd9:a8a236a1@us-cdbr-iron-east-03.cleardb.net/heroku_7617e6dc46c4343?reconnect=true'
 
 const connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-03.cleardb.net',
   user: 'bc321ec3408dd9',
   password: 'a8a236a1',
-  database: 'us-cdbr-iron-east-03.cleardb.net'
+  database: 'heroku_7617e6dc46c4343'
 });
 
 const selectAll = (callback) => {
