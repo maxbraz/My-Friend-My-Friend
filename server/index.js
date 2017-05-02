fae15103f50f5a902a8c8d7c969d5619e205b54b
Max Braz <braz.max@gmail.com>
2017-05-02 12:19:18 -0700

    delete procfile.  change packageJSON start.

--- server/index.js
+++ server/index.js
@@ -59,5 +59,5 @@ app.post('/question', (req, res) => {
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('listening on port ', port);
+  console.log(`listening on port ${port}`);
 });
